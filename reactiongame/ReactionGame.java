9ce1a7350ba00bd809666451cc4a03db69e06209
AarnavR <Aarnav.Rawat@gmail.com>
2023-06-24 14:48:31 -0400

    “…”

--- reactiongame/ReactionGame.java
+++ reactiongame/ReactionGame.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 public class ReactionGame {
     public static void main(String[] args) throws InterruptedException{
 
-
-        final long startTime = System.currentTimeMillis();
-
-        System.out.println("Rewrite the following word below: 'purple' ");
+        System.out.println("Rewrite the following word text: \n 'According to the National Center for Voice and Speech, the average conversation rate for English speakers in the United States is about 150 wpm.' ");
         Thread.sleep(1000);
         System.out.println("3");
         Thread.sleep(1000);
@@ -18,12 +15,13 @@ public class ReactionGame {
         Thread.sleep(1000);
         Scanner scanner = new Scanner(System.in);
         System.out.print("Write the text: ");
+        final long startTime = System.currentTimeMillis();
         String text = scanner.nextLine();
-        if (text.equals("purple")){
+        if (text.equals("According to the National Center for Voice and Speech, the average conversation rate for English speakers in the United States is about 150 wpm.")){
             final long endTime = System.currentTimeMillis();
-            System.out.println("You took " + ((double)endTime - (double)startTime)/ (double)1000 + " seconds to write the word");
+            System.out.println("You took " + ((double)endTime - (double)startTime)/ ((double)1000 * 24) + " seconds per each word");
         } else {
-            System.out.println("You mispelled the word :(");
+            System.out.println("You mispelled something :( )");
         }
 
     }

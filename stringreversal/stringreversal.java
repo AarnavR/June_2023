94edb4ae0771a94e8d924bf7d8b6a3de16098058
AarnavR <Aarnav.Rawat@gmail.com>
2023-06-24 14:12:20 -0400

    finished reverse

--- stringreversal/stringreversal.java
+++ stringreversal/stringreversal.java
@@ -1,15 +1,19 @@
 package stringreversal;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class stringreversal {
-    String reverse(String in){
+    static String reverse(String in){
         String s = "";
+        for (int i = 0; i < in.length(); i++){
+            s += in.charAt(in.length() - i - 1);
+        }
         return s;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to reverse: ");
-        String guess = scanner.nextLine();
-
+        System.out.print("Enter a string to reverse: ");
+        String str = scanner.nextLine();
+        System.out.println(reverse(str));
     }
 }
